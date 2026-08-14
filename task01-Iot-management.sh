60c7d3d3e5c3300c39c77ae594b3fdf6c0bce9cc
HashenRubix <hashenpersonal@gmail.com>
2026-08-14 12:40:18 +0530

    add cpu and memory usage to task 01

--- task01-Iot-management.sh
+++ task01-Iot-management.sh
@@ -1,2 +1,15 @@
 #! /bin/bash
 
+#process monitoring and management tool
+echo "====== Process Monitoring Tool ======"
+
+#cpu usage
+echo "Current cpu usage"
+top -bn1 | grep "Cpu(s)"
+
+echo "======================="
+
+#memory usage
+echo "Current memory usage"
+free -h
+
