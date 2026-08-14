48c3147cf1edc115ab9b265f98d90f31321ea594
HashenRubix <hashenpersonal@gmail.com>
2026-08-14 13:00:13 +0530

    add memory cnsuming and pid process

--- task01-Iot-management.sh
+++ task01-Iot-management.sh
@@ -13,3 +13,23 @@ echo "======================="
 echo "Current memory usage"
 free -h
 
+echo "======================"
+
+#display top 10 memory consuming process
+echo "Top 10 memory consumin processes"
+ps aux --sort=-%mem | head -n 11
+
+#ask for a pid
+
+echo ""
+read -p "Enter the PID of the process you want to terminate : " PID
+
+#check wether PID
+
+if ! ps -p "$PID" >/dev/null
+then
+	echo "process with PID $PID dose not exist."
+	exit 1
+fi
+
+
