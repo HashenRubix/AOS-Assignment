70e9ebf0ef953632e8290b101492fb4092c56d30
HashenRubix <hashenpersonal@gmail.com>
2026-08-17 15:45:10 +0530

    add main menu

--- task02-Job_scheduler.sh
+++ task02-Job_scheduler.sh
@@ -46,3 +46,25 @@ fi
 # Record program start
 log_action "University Research Cluster Job Scheduler started"
 
+# MAIN MENU
+# ==========================================================
+
+while true
+do
+
+
+    echo "=============================================="
+    echo "   UNIVERSITY RESEARCH CLUSTER JOB SCHEDULER"
+    echo "=============================================="
+    echo "1. View Pending Jobs"
+    echo "2. Submit a Job Request"
+    echo "3. Process Queue - Round Robin"
+    echo "4. Process Queue - Priority Scheduling"
+    echo "5. View Completed Jobs"
+    echo "6. Exit System"
+    echo "=============================================="
+
+    read -p "Enter your choice: " CHOICE
+#memo:- tempory end. start here
+done
+
